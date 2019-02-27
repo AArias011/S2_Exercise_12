30a7cb2f6a9e966b82c9175920944f54ad7d98e5
Anthony Arias <aarias011@west-mec.org>
2019-02-26 18:18:20 -0700

    Final commit push. Ready for review.
    Today I made a function to write the table rows for each day of the month, determined the starting day of the month, wrote blank cells preceding the starting day, and then lastly wrote cells for each day of the month.

--- lht_calendar.js
+++ lht_calendar.js
@@ -63,7 +63,7 @@
 // }
 
 /* Set the date displayed in the calender*/
-var thisDay = new Date("August 24, 2018");
+var thisDay = new Date();
 
 // Write the calender to the element to the id "calender"
 document.getElementById("calendar").innerHTML = createCalendar(thisDay);
@@ -74,6 +74,7 @@ function createCalendar(calDate) {
       var calendarHTML = "<table id='calendar_table'>";
       calendarHTML += calCaption(calDate);
       calendarHTML += calWeekdayRow();
+      calendarHTML += calDays(calDate);
       calendarHTML += "</table>";
       return calendarHTML;
 }
@@ -90,8 +91,8 @@ function calCaption(calDate) {
       var thisYear = calDate.getFullYear();
 
       //write the caption
-      return "<caption>" + monthName[thisMonth] + " "
-      thisYear + "</caption>";
+      return "<caption>" + monthName[thisMonth] + " " +
+            thisYear + "</caption>";
 }
 
 // function to write a table row of weekday abbreviations
@@ -119,9 +120,51 @@ function daysInMonth(calDate) {
 
       //revise the days in February for leap years
       if (thisYear % 4 === 0) {
-            dayCount[1] = 29;
+            if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+                  dayCount[1] = 29;
+            }
       }
 
       //return the number of days for the current month
-      return dayCount(thisMonth);
+      return dayCount[thisMonth];
+}
+
+// function to write the table rows for each day of the month
+function calDays(calDate) {
+      //determine the starting day of the month
+      var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+      var weekDay = day.getDay();
+      //write blank cells preceding the starting day
+      var htmlCode = "<tr>";
+      for (var i = 0; i < weekDay; i++) {
+            htmlCode += "<td></td>";
+      }
+      //write cells for each day of the month
+      var totalDays = daysInMonth(calDate);
+      var highlightDay = calDate.getDate();
+      for (var i = 1; i <= totalDays; i++) {
+            day.setDate(i);
+            weekDay = day.getDay();
+
+            //if statements
+            if (weekDay === 0) {
+                  htmlCode += "<tr>";
+            }
+
+
+
+
+            if (i === highlightDay) {
+                  htmlCode += "<td class='calendar_dates' id='calendar_today'>" + i + dayEvent[i] + "</td>";
+            } else {
+                  htmlCode += "<td class='calendar_dates'>" + i + dayEvent[i] + "</td>";
+            }
+
+
+
+            if (weekDay === 6) {
+                  htmlCode += "</tr>";
+            }
+      }
+      return htmlCode;
 }
